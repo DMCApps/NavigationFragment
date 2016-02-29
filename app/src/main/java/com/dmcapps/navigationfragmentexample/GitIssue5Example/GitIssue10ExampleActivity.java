b968422a9c569bd7b8a688d933e8ebc20404b389
世外桃源 <jobhesk@gmail.com>
2016-02-29 18:07:44 +0800

    adding library
    discovered that the flashes in the viewpager.

---
+++ app/src/main/java/com/dmcapps/navigationfragmentexample/GitIssue5Example/GitIssue10ExampleActivity.java
@@ -0,0 +1,13 @@
+package com.dmcapps.navigationfragmentexample.GitIssue5Example;
+
+import com.dmcapps.navigationfragment.fragments.NavigationFragment;
+
+/**
+ * Created by hesk on 29/2/16.
+ */
+public class GitIssue10ExampleActivity extends basicImplementation {
+    @Override
+    protected NavigationFragment initFragment() {
+        return SmartTabLayout.newInstance();
+    }
+}
